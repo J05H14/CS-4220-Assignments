030784b52564e5ffa06c39b777851c506588e1e6
JAUSH <j05h14@yahoo.com>
2019-02-06 13:33:56 -0800

    hw1 question 2 complete

--- HW 1/hw1.js
+++ HW 1/hw1.js
@@ -11,8 +11,27 @@ function replaceLetters(str, arr){
 
 //Testing Question 1
 console.log('Question 1 Test')
-const arr = ['a', 'e', 'i', 'o', 'u' ]
+const arr1 = ['a', 'e', 'i', 'o', 'u' ]
 const s = 'aerial'
 console.log('original word: ' + s)
-const replacedS = replaceLetters(s, arr)
+const replacedS = replaceLetters(s, arr1)
 console.log('replaceLetters:' +replacedS)
+
+
+//2. Write a function called sumArray.
+function sumArray(arr){
+	let sum = 0
+	arr.forEach(elem => {
+		if(!isNaN(elem) && typeof(elem) != typeof(true)){
+			sum += parseInt(elem)
+		}
+	})
+	return sum
+}
+
+//Testing Question 2
+console.log('Question 2 Test')
+const arr2 = [5, 2, 'a', 4, '7', true, 'b', 'c', 7, '8', false]
+const sumArr2 = sumArray(arr2)
+console.log('Original Array: ' + arr2)
+console.log('SumArray: ' + sumArr2)
