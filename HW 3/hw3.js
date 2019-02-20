f3d16f1bbb531196c31abcd79fec3015da0793e1
JAUSH <j05h14@yahoo.com>
2019-02-20 13:35:00 -0800

    check priority for hw3

--- HW 3/hw3.js
+++ HW 3/hw3.js
@@ -1,28 +1,32 @@
 //1. Using async/await syntax write a set of functions that adds numbers and awaits for the total.
 //Promise to add two numbers with a given time
-const add = ({first, second, time} ) => {
+const add = ({
+    first,
+    second,
+    time
+}) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             //checks to make sure the inputs are numbers
-            const error1 = isNaN(first) ? `${first} is not a number` : null
-            const error2 = isNaN(second) ? `${second} is not a number` : null
-            
+            const error1 = isNaN(first) ? `${first} is not a number` : null;
+            const error2 = isNaN(second) ? `${second} is not a number` : null;
+
             //handling the NaN error
-            if(error1 || error2){
-                if(error1){
-                    reject(error1)
+            if (error1 || error2) {
+                if (error1) {
+                    reject(error1);
                 }
-                if(error2){
-                    reject(error2)
+                if (error2) {
+                    reject(error2);
                 }
             }
             //Adding the numbers up
-            else{
-                resolve(first + second)
+            else {
+                resolve(first + second);
             }
-        })
-    }, time)
-}
+        });
+    }, time);
+};
 
 //adder function to call the promise so that it only needs the numbers, not the time
 const adder = (previous, next) => {
@@ -30,34 +34,79 @@ const adder = (previous, next) => {
     //     .then((result) => {
     //         console.log(result)
     //     })
-    return add({first: previous, second: next, time: 10})
-}
+    return add({
+        first: previous,
+        second: next,
+        time: 10
+    });
+};
 
 //interateNumbers async function
-async function iterateNumbers(numbers){
-    let total = 0
-    for(let i = 0; i < numbers.length; i += 2){
-        const current = numbers[i]
-        let next
+async function iterateNumbers(numbers) {
+    let total = 0;
+    for (let i = 0; i < numbers.length; i += 2) {
+        const current = numbers[i];
+        let next;
 
         //if else for the case that there are an odd number of elements
-        if(!(i == numbers.length - 1)){
-          next = numbers[i+1]
+        if (i != numbers.length - 1) {
+            next = numbers[i + 1];
+        } else {
+            next = 0;
         }
-        else{
-          next = 0
-        }
-        try{
-            const add2 = await adder(current, next)
+        try {
+            const add2 = await adder(current, next);
             // console.log(`add: ${add2}`)
-            total += add2
-        }catch(err){
-            console.log(err)
+            total += add2;
+        } catch (err) {
+            console.log(err);
         }
     }
-    console.log(`Total:\n${total}`)
+    console.log(`Total:\n${total}`);
 }
 
 //Testing Question 1
 console.log("Question 1 Test");
-iterateNumbers([1, 2, 3, 5, 8, 13, 21])
+iterateNumbers([1, 2, 3, 5, 8, 13, 21]);
+
+//2. Using callbacks syntax write a set of functions that checks and makes a list of todos based on priorities.
+//check proority function
+const checkPriority = (todo, callback) => {
+    const {name, priority} = todo;
+    setTimeout(() => {
+        //error for case if no priority
+        const error = !priority ? `${name} has no priority` : null;
+        callback(error, todo);
+    }, 90);
+};
+
+//Testing Question 2
+console.log("Question 2 Test");
+const todos = [{
+        name: 'get coffee',
+        priority: 9
+    },
+    {
+        name: 'clean room',
+        priority: null
+    },
+    {
+        name: 'go to CS4220',
+        priority: 4
+    },
+    {
+        name: 'do homework before due date',
+        priority: 8
+    }
+];
+
+checkPriority(todos[0], (err, result)=>{
+    console.log(result);
+});
+// makePriorityList(todos);
+
+// Priority
+//   [ { name: 'get coffee', priority: 90 },
+//   { name: 'do homework before due date', priority: 80 },
+//   { name: 'go to CS4220', priority: 40 } ]
+// Missing Priority [ 'clean room' ]
