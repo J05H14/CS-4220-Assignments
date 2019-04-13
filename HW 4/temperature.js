ade2c952c90f0c0c76351a22694785aeb498e30c
JAUSH <j05h14@yahoo.com>
2019-04-13 10:51:21 -0700

    fixed formatting

--- HW 4/temperature.js
+++ HW 4/temperature.js
@@ -1,94 +1,96 @@
-window.onload = function(){
-    const app = document.getElementById('root')
+const app = document.getElementById("root")
+const title = document.createElement("div")
 
-    const title = document.createElement('div')
+const instructions = document.createElement("h1")
+instructions.textContent = "Enter the Temperature to be Converted"
 
-    const instructions = document.createElement('h1')
-    instructions.textContent = 'Enter the Temperature to be Converted'
+const inp = document.createElement("input")
+inp.setAttribute("type", "text")
+inp.setAttribute("id", "temp")
 
-    const inp = document.createElement('input')
-    inp.setAttribute('type', 'text')
-    inp.setAttribute('id', 'temp')
+const tempPick = document.createElement("div")
 
-    const tempPick = document.createElement('div')
+const radioF = document.createElement("input")
+radioF.setAttribute("type", "radio")
+radioF.setAttribute("id", "toF")
+radioF.setAttribute("name", "unit")
+radioF.setAttribute("value", "to Fahrenhiet")
 
-    const radioF = document.createElement('input')
-    radioF.setAttribute('type', 'radio')
-    radioF.setAttribute('id', 'toF')
-    radioF.setAttribute('name', 'unit')
-    radioF.setAttribute('value', 'to Fahrenhiet')
+const radioFLabel = document.createElement("FLabel")
+radioFLabel.textContent = "to Fahrenheit"
 
-    const radioFLabel = document.createElement('FLabel')
-    radioFLabel.textContent = 'to Fahrenheit'
+const radioC = document.createElement("input")
+radioC.setAttribute("type", "radio")
+radioC.setAttribute("id", "toC")
+radioC.setAttribute("name", "unit")
 
-    const radioC = document.createElement('input')
-    radioC.setAttribute('type', 'radio')
-    radioC.setAttribute('id', 'toC')
-    radioC.setAttribute('name', 'unit')
-    
-    const radioCLabel = document.createElement('CLabel')
-    radioCLabel.textContent = 'to Celcius'
+const radioCLabel = document.createElement("CLabel")
+radioCLabel.textContent = "to Celcius"
 
+const btnDiv = document.createElement("div")
 
-    const button = document.createElement('input')
-    button.setAttribute('type', 'button')
-    button.setAttribute('value', 'Submit')
-    button.setAttribute('id', 'btnConvert')
-    button.setAttribute('onclick', 'convert()')
+const button = document.createElement("input")
+button.setAttribute("type", "button")
+button.setAttribute("value", "Submit")
+button.setAttribute("id", "btnConvert")
+button.setAttribute("onclick", "convert()")
 
-    const out = document.createElement('h2')
+const out = document.createElement("h2")
 
-    const output = document.createElement('output')
+const output = document.createElement("output")
 
-    title.appendChild(instructions)
-    title.appendChild(inp)
-    
-    tempPick.appendChild(radioF)
-    tempPick.appendChild(radioFLabel)
-    tempPick.appendChild(radioC)
-    tempPick.appendChild(radioCLabel)
-    tempPick.appendChild(button)
-    out.appendChild(output)
-    app.appendChild(title)
-    app.appendChild(tempPick)
-    app.appendChild(out)
+title.appendChild(instructions)
+title.appendChild(inp)
 
-    const convert = () => {
-        const temp = document.getElementById('temp').value
-        const toC = document.getElementById('toC')
-        const toF = document.getElementById('toF')
+tempPick.appendChild(radioF)
+tempPick.appendChild(radioFLabel)
+tempPick.appendChild(radioC)
+tempPick.appendChild(radioCLabel)
+btnDiv.appendChild(button)
+out.appendChild(output)
+app.appendChild(title)
+app.appendChild(tempPick)
+app.appendChild(btnDiv)
+app.appendChild(out)
 
-        console.log(temp)
-        let result = 0
+const convert = () => {
+    const temp = document.getElementById("temp").value
+    const toC = document.getElementById("toC")
+    const toF = document.getElementById("toF")
 
-        if(temp == '' || (!toC.checked && !toF.checked)){
-            const blankErr = 'Must Input a Temperature AND Select a Unit'
-            document.querySelector('output').innerHTML = blankErr
-        }
-        else if(temp != '' & isNaN(temp)){
-            const nanErr = 'Input MUST be a Number'
-            document.querySelector('output').innerHTML = nanErr
-        }
+    let output = 0
+    let result = 0
 
-        else{
-            // result = (temp - 32) / 1.8
-            if(toC.checked){
-                console.log('to celcius')
-                result = (parseInt(temp) - 32) / 1.8
-                console.log(result)
-            }
-            else{
-                console.log('to fahrenheit')
-                result = (parseInt(temp) * 1.8) + 32
-                console.log(result)
-            }
-
+    if (temp == "" || (!toC.checked && !toF.checked)) {
+        const blankErr = "Must Input a Temperature AND Select a Unit"
+        document.querySelector("output").innerHTML = blankErr
+    } else if ((temp != "") & isNaN(temp)) {
+        const nanErr = "Input MUST be a Number"
+        document.querySelector("output").innerHTML = nanErr
+    } else {
+        // result = (temp - 32) / 1.8
+        if (toC.checked) {
+            console.log("to celcius")
+            result = (parseInt(temp) - 32) / 1.8
             console.log(result)
-            
-            const output = result
-            document.querySelector('output').innerHTML = output
+
+            output = temp + " F is " + result + " C"
+        } else {
+            console.log("to fahrenheit")
+            result = parseInt(temp) * 1.8 + 32
+            console.log(result)
+            output = temp + " C is " + result + " F"
         }
+
+        console.log(result)
+
+        document.querySelector("output").innerHTML = output
     }
-    
-    document.getElementById('btnConvert').addEventListener('click', function(){convert('F')}, false)
-}
+};
+
+document.getElementById("btnConvert").addEventListener(
+    "click",
+    function () {
+        convert()
+    }
+);
